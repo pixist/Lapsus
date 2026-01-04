96fc9f3108e858eaf4bde0dc800775d411ab3c52
margooey <234476012+margooey@users.noreply.github.com>
2026-01-03 22:36:04 -0500

    don't import all of cg in utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use cidre::cg;
+use cidre::cg::{Rect, Point, Size};
 
 pub fn min(a: f64, b: f64) -> f64 {
     if a > b { b } else { a }
@@ -8,11 +8,11 @@ pub fn max(a: f64, b: f64) -> f64 {
     if a < b { b } else { a }
 }
 
-pub fn union_rect(a: &cg::Rect, b: &cg::Rect) -> cg::Rect {
-    if *a == cg::Rect::null() {
+pub fn union_rect(a: &Rect, b: &Rect) -> Rect {
+    if *a == Rect::null() {
         return *b;
     }
-    if *b == cg::Rect::null() {
+    if *b == Rect::null() {
         return *a;
     }
     let min_x = min(a.origin.x, b.origin.x);
@@ -20,9 +20,9 @@ pub fn union_rect(a: &cg::Rect, b: &cg::Rect) -> cg::Rect {
     let max_x = max(a.origin.x + a.size.width, b.origin.x + b.size.width);
     let max_y = max(a.origin.y + a.size.height, b.origin.y + b.size.height);
 
-    cg::Rect {
-        origin: cg::Point { x: min_x, y: min_y },
-        size: cg::Size {
+    Rect {
+        origin: Point { x: min_x, y: min_y },
+        size: Size {
             width: max_x - min_x,
             height: max_y - min_y,
         },
